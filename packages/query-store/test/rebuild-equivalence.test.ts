42627a62a592903f0cbc37c5802fe19d106471cd
Javier Rodríguez González <kaslabdevs@gmail.com>
2026-05-19 13:55:27 +0200

    [KLD]:0.5.3-alpha version

--- packages/query-store/test/rebuild-equivalence.test.ts
+++ packages/query-store/test/rebuild-equivalence.test.ts
@@ -36,7 +36,7 @@ describe("Query Store Rebuild Equivalence", () => {
     const artifact: any = {
       schema,
       version: "1.0.0-alpha",
-      hardkasVersion: "0.5.2-alpha",
+      hardkasVersion: "0.5.3-alpha",
       networkId: "simnet",
       mode: "real",
       artifactId: id,
