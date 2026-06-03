757d2fc90493ea577a3b2bf08fea9928532efc39
KasLab <kaslabdevs@gmail.com>
2026-06-04 00:32:52 +0200

    Delete .smoke-workspace directory

--- .smoke-workspace/test-imports.mjs
+++
@@ -1,8 +0,0 @@
-
-import config from '@hardkas/config' catch(e) {};
-import core from '@hardkas/core' catch(e) {};
-import artifacts from '@hardkas/artifacts' catch(e) {};
-import sdk from '@hardkas/sdk' catch(e) {};
-import cli from '@hardkas/cli' catch(e) {};
-
-console.log("✅ All ESM raw imports succeeded!");
