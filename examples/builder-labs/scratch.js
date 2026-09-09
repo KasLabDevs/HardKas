e6437fccec3e7e5f8e3c2e88a3c5fae9f9e57c43
Javier Rodríguez González <kaslabdevs@gmail.com>
2026-09-09 10:48:10 +0200

    [KLD]: 0.12.0-rc.20 version

--- examples/builder-labs/scratch.js
+++
@@ -1,5 +0,0 @@
-const { PrivateKey } = require("./packages/kaspa-wasm");
-const priv = new PrivateKey("0000000000000000000000000000000000000000000000000000000000000001");
-const pub = priv.toKeypair().publicKey;
-console.log("pubkey length:", pub.length);
-console.log("pubkey:", pub);
